56098602103b6dadc76ea1ff5dfdba82eba99fc1
Ismail M <ismail.moghul@gmail.com>
2014-08-29 12:07:21 +0100

    update

--- lib/GeneValidatorApp.rb
+++ lib/GeneValidatorApp.rb
@@ -5,11 +5,13 @@ require 'fileutils'
 module GeneValidatorApp
 
   def create_unique_name
+    puts 'creating a unique name'
     unique_name = Time.new.strftime('%Y-%m-%d_%H-%M-%S-%L-%N') + '_' + request.ip.gsub('.','-')
     return unique_name
   end
 
   def ensure_unique_name(public_folder)
+    puts 'Ensuring the run has a unique name'
     while File.exist?(public_folder)
       unique_name    = create_unique_name
       working_folder = File.join(Dir.home + '/Genevalidator/' + unique_name)
@@ -18,6 +20,7 @@ module GeneValidatorApp
   end
 
   def to_fasta(sequence)
+    puts 'Converting Sequences to Fasta format if necessary.'
     sequence = sequence.lstrip
     unique_queries = Hash.new()
     if sequence[0,1] != '>'
@@ -35,13 +38,20 @@ module GeneValidatorApp
     return sequence
   end
 
+  def create_fasta_file(working_folder, sequences)
+    puts 'Writing the input sequences into a fasta file.'
+    File.open(File.join(working_folder, "input_file.fa"), 'w+') do |f|
+      f.write sequences
+    end
+  end
+
   def run_genevalidator (validation_array, working_folder, public_folder, unique_name)
     index_folder = File.join(working_folder, 'input_file.fa.html')
 
     puts 'Running Genevalidator from a sub-shell'
     command = "time Genevalidator -v \"#{validation_array}\" #{working_folder}/input_file.fa"
     exit = system(command)
-    raise IOError("Genevalidator exited with the command code: #{exit}") unless exit
+    raise IOError, "Genevalidator exited with the command code: #{exit}" unless exit
 
     html_table = extract_table_html(working_folder, public_folder, unique_name)
 
@@ -50,7 +60,7 @@ module GeneValidatorApp
 
   def extract_table_html(working_folder, public_folder, unique_name)
     index_file = File.join(working_folder, "/input_file.fa.html", "index.html")
-    raise IOError("GeneValidator has not created any results files...") unless File.exist?(index_file)
+    raise IOError, "GeneValidator has not created any results files..." unless File.exist?(index_file)
 
     puts 'Reading the html output file...'
     full_html = IO.binread(index_file)
@@ -65,20 +75,30 @@ module GeneValidatorApp
     return @html_table
   end
 
-  def write_html_table(public_folder, html_table)
-    puts 'Writing the table to a file'
-    File.open("#{public_folder}/table.html", 'w') do |f|
-      f.write html_table
-    end
-  end
-
   def copy_json_plots(working_folder, public_folder)
     puts "copying Json files"
     json_files = File.join(working_folder, "/input_file.fa.html", "*.json")
     FileUtils.cp_r Dir.glob(json_files), public_folder
   end
 
+  def write_html_table(public_folder, html_table)
+    puts 'Writing the table to a file'
+    table_file = File.join(public_folder, 'table.html')
+    File.open(table_file, 'w') do |f|
+      f.write html_table
+    end
+  end
+
+  def read_table(public_folder)
+    puts 'Reading the table file'
+    table_file = File.join(public_folder, 'table.html')
+    raise IOError, 'The table file cannot be found.' unless File.exist?(table_file)
+    results = IO.read(table_file)
+
+  end
+
   def create_results(insides)
+    puts 'creating results'
     results = '<div id="results_box"><h2 class="page-header">Results</h2>'+ insides + '</div>'
     return results
   end
