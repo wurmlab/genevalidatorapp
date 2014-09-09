fbaf2033d06f75324a9c88e17e2c605243e18570
Ismail M <ismail.moghul@gmail.com>
2014-09-09 19:28:13 +0100

    updates

--- lib/GeneValidatorApp.rb
+++ lib/GeneValidatorApp.rb
@@ -53,8 +53,8 @@ module GeneValidatorApp
   # Runs GeneValidator from the command line and return just the table html...
   def run_genevalidator(validations, db, working_dir, unique_name)
     index_file = File.join(working_dir, 'input_file.fa.html', 'index.html')
-    command    = 'Genevalidator -v "' + validations + '" -d ' + db + ' ' +
-                  File.join(working_dir, 'input_file.fa')
+    command    = 'Genevalidator -v "' + validations + '" -d "' + db + '" "' +
+                  File.join(working_dir, 'input_file.fa') + '"'
     exit       = system(command)
     unless exit
       raise IOError, "Genevalidator exited with the command code: #{exit}."
@@ -91,4 +91,142 @@ module GeneValidatorApp
               insides + '</div>'
     return results
   end
+end
+
+
+
+#### Perhaps put the below in a new file, in a new module...
+
+
+### Runs GeneValidator with a small test case... If GeneValidator exits with the 
+#   the right exit code, it is assumed that it works perfectly. This also tests
+#   the Tempdir is writable.... 
+def check_genevalidator_works(temp_dir, db_array)
+  #test_unit = File.join(.......)
+
+    # command    = 'Genevalidator -v "' + validations + '" -d ' + db + ' ' +
+    #               File.join(working_dir, 'input_file.fa')
+    # exit       = system(command)
+
+    # unless exit
+    #   puts "*** Genevalidator exited with the command code: #{exit}."
+    #   exit
+    # end
+
+  
+end
+
+
+
+##### The below has been adapted from SequenceServer
+
+### Obtain a array of dbs 
+
+  # Scan the given directory (including subdirectory) for blast databases.
+  # ---
+  # Arguments:
+  # * db_root(String) - absolute path to the blast databases
+  # ---
+  # Returns:
+  # * a hash of sorted blast databases indexed by their id.
+  def scan_blast_database_directory(db_root)
+    find_dbs_command = %|blastdbcmd -recursive -list #{db_root} -list_outfmt "%p %f %t" 2>&1|
+
+    db_list = %x|#{find_dbs_command}|
+    if db_list.empty?
+      puts "*** No formatted blast databases found in '#{db_root}'."
+      puts "    Please ensure that there are BLAST database in the #{db_root}."
+      exit
+    elsif db_list.match(/BLAST Database error/)
+      puts "*** Error parsing one of the BLAST databases."
+      puts "    Mostly likely some of your BLAST databases were created by an old version of 'makeblastdb'."
+      puts "    You will have to manually delete problematic BLAST databases and subsequently use the latest version of BLAST + to create new ones."
+      exit
+    elsif not $?.success?
+      puts "*** Error obtaining BLAST databases."
+      puts "    Tried: #{find_dbs_command}"
+      puts "    Error:"
+      db_list.strip.split("\n").each { |l| puts "\t#{l}"}
+      puts "    Please could you report this to '....Link...'"
+      exit
+    end
+
+    db = {}
+    db_list.each_line do |line|
+      next if line.empty?  # required for BLAST+ 2.2.22
+      type, name, *title =  line.split(' ')
+      type  = type.downcase.intern
+      name  = name.freeze
+      title = title.join(' ').freeze
+      # # skip past all but alias file of a NCBI multi-part BLAST database
+      # if multipart_database_name?(name)
+      #   puts "Found a multi-part database volume at #{name} - ignoring it."
+      #   # logger.info(%|Found a multi-part database volume at #{name} - ignoring it.|)
+      #   next
+      # end
+
+      db[title] = [name: name, type:type]
+
+      puts "Found #{type} database: #{title} at #{name}"
+      # logger.info("Found #{type} database: #{title} at #{name}")
+    end
+    puts db
+    db
+  end
+
+
+
+### Ensures that GV is installed and is of the correct version...
+def assert_gv_installed_and_compatible()
+  unless command? 'genevalidator'
+    puts "*** Could not find GeneValidator."
+    puts "    Please Confirm that you have GeneValidator installed and try again. "
+    puts "    Please refer to ...Link... for more information."
+    exit
+  end
+  ### Add the --version argument to GeneValidator
+  # version = %x|genevalidator -version|.split[1]
+  # unless version >= '1.0'
+  #   puts "*** Your GeneValidator version #{version} is outdated."
+  #   puts "    This App require GeneValidator version 1.0 or higher."
+  #   exit
+  # end
+end
+
+# check if the given command exists and is executable
+# returns True if all is good.
+def command?(command)
+  system("which #{command} > /dev/null 2>&1")
+end
+
+
+def choose_default(databases)
+  db_titles = []
+  databases.each do |title, hash|
+    db_titles.push(title)
+  end
+
+  raise IOError, ':( Something went wrong ' if databases.length != db_titles.length
+
+  puts # a blank line
+  puts "#{databases.length} databases have found."
+  puts # a blank line
+
+  db_number = 0
+  while db_number < db_titles.length
+    puts "[#{db_number + 1}]:  #{db_titles[db_number]}"
+    db_number += 1
+  end
+
+  puts # a blank line
+  puts "Please choose your default database. (Pick a number between 1 and #{db_titles.length}) "
+  print '> '
+  inp = $stdin.gets.chomp
+  i = inp.to_i - 1
+  default_db = db_titles[i]
+  puts "You have chosen #{default_db} as your default database."
+  puts # a blank line
+
+  
+  return default_db
 end
--- public/js/genevalidator.js
+++ public/js/genevalidator.js
@@ -1,63 +1,43 @@
 $(document).ready(function() {
-  check_collapse_state();
+  checkCollapseState()
 
   $('#input').submit(function(e) {
-    e.preventDefault();
+    e.preventDefault()
     // show activity spinner
     $('#spinner').modal({
-        backdrop: 'static',
-        keyboard: 'false'
-      });
+      backdrop: 'static',
+      keyboard: 'false'
+    })
 
     // Check the type of the sequence...
     // check numbers
     // if (checkInputSeq()) {
-    //   $('#spinner').modal('hide');
+    //   $('#spinner').modal('hide')
     //   //  Load an error Modal...
-    //   return;
+    //   return
     // }
 
     // Ckeck if no  Validations 
     if (checkEmptyValidation()) {
-      $('#spinner').modal('hide');
+      $('#spinner').modal('hide')
       //  Load an error Modal...
-      return;
-    };
-
-    // DO Valdiations here before sending off the request...
+      return
+    }
 
     $.ajax({
       type: 'POST',
       url: '/input',
       data: $('#input').serialize(),
       success: function(response){
-        $('#output').html(response);
+        $('#output').html(response)
         // initiate the table sorter
-             // add custom parser to make the stars column to sort according to attr.
-            $.tablesorter.addParser({
-              id: 'star_scores', // called later when init the tablesorter
-              is: function(s) {return false;},
-              format: function(s, table, cell, cellIndex) {
-                var $cell = $(cell);
-                if (cellIndex === 2) {
-                  return $cell.attr('data-score') || s;
-                } 
-                return s;
-              },
-              parsed: false,
-              type: 'numeric'
-            });
-
-            $('table').tablesorter({
-              headers: {
-                2 : { sorter: 'star_scores' } // Telling it to use custom parser...
-              },
-            });
-
+        initTableSorter()
         // Initiate the tooltips
-        $("[data-toggle='tooltip']").tooltip();
+        $("[data-toggle='tooltip']").tooltip()
+        // Remove Unwanted Columns
+        removeEmptyColumns(); 
         // remove progress notification
-        $('#spinner').modal('hide');
+        $('#spinner').modal('hide')
       },
     })
   })
@@ -65,9 +45,9 @@ $(document).ready(function() {
   // Handles the form submission when Ctrl+Enter is pressed anywhere on page. taken from SequenceServer...
   $(document).bind("keydown", function (e) {
     if (e.ctrlKey && e.keyCode === 13 ) {
-      $('#input').trigger('submit');
+      $('#input').trigger('submit')
     }
-  });
+  })
 })
 
 
@@ -78,10 +58,10 @@ $(document).ready(function() {
 //// No Numbers....
 function checkInputSeq(){
     var fasta = require('bionode-fasta')
-    var seq = require('bionode-seq');
-    var input = document.forms["input"]['seq'].value;
+    var seq = require('bionode-seq')
+    var input = document.forms["input"]['seq'].value
     // var type = seq.checkType(input)
-    // console.log(type);
+    // console.log(type)
     // console.log("hi")
 
     // fasta.obj('seq').on('data', console.log) // Returns Objects
@@ -94,43 +74,77 @@ function checkInputSeq(){
 
 
 function checkEmptyValidation() {
-  var val = document.forms["input"]['validations[]'];
-  var checkedVal = [];
+  var val = document.forms["input"]['validations[]']
+  var checkedVal = []
   var valLength = val.length
 
   for (var i = 0; i < valLength; i++) {
     if (val[i].checked) {
-      checkedVal.push(val[i]);
+      checkedVal.push(val[i])
     }
   }
-  return checkedVal.length === 0 ? true : false; 
+  return checkedVal.length === 0 ? true : false
 }
 
 
-function change_adv_params_btn_text(adv_user){
-  var btn = document.getElementById("adv_params_btn");
+function ChangeAdvParamsBtnText() {
+  var btn = document.getElementById("adv_params_btn")
   if (btn.innerHTML === '<i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Show Advanced Parameters') {
-    btn.innerHTML = '<i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Hide Advanced Parameters';
-    $('#adv_params').collapse('show');
-    $.cookie('adv_params_status', 'open');
+    btn.innerHTML = '<i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Hide Advanced Parameters'
+    $('#adv_params').collapse('show')
+    $.cookie('adv_params_status', 'open')
   }
   else {
-    btn.innerHTML = '<i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Show Advanced Parameters';
-    $('#adv_params').collapse('hide');
-    $.cookie('adv_params_status', 'closed');
-  };
+    btn.innerHTML = '<i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Show Advanced Parameters'
+    $('#adv_params').collapse('hide')
+    $.cookie('adv_params_status', 'closed')
+  }
 }
 
 
-function check_collapse_state() {
+function checkCollapseState() {
   if ($.cookie('adv_params_status')){
-    var adv_params_status = $.cookie('adv_params_status');
+    var adv_params_status = $.cookie('adv_params_status')
     if (adv_params_status === 'open') {
-        var btn = document.getElementById("adv_params_btn");
-        btn.innerHTML = '<i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Hide Advanced Parameters';
-        $('#adv_params').addClass('in');
-    };
-  };
+        var btn = document.getElementById("adv_params_btn")
+        btn.innerHTML = '<i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Hide Advanced Parameters'
+        $('#adv_params').addClass('in')
+    }
+  }
 }
 
+// A Table sortert Initialiser 
+//   Contains a custom parser that allows the Stars to be sorted. 
+function initTableSorter() {
+  $.tablesorter.addParser({
+    id: 'star_scores',
+    is: function(s) {return false},
+    format: function(s, table, cell, cellIndex) {
+      var $cell = $(cell)
+      if (cellIndex === 2) {
+        return $cell.attr('data-score') || s
+      } 
+      return s
+    },
+    parsed: false,
+    type: 'numeric'
+  })
 
+  $('table').tablesorter({
+    headers: {
+      2 : { sorter: 'star_scores' }
+    },
+  })
+}
+
+function removeEmptyColumns() {
+  $('#sortable_table tr th').each(function(i) {
+    var tds = $(this).parents('table') // Select all tds in column
+    .find('tr td:nth-child(' + (i + 1) + ')')
+    // Check if all cells in the column are empty
+    if(tds.length == tds.filter(':empty').length) { 
+      $(this).hide() // Hide header
+      tds.hide() // Hide cells
+    }
+  })
+}
