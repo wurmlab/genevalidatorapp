7a7f30464d0caeccbdd8de1a8d1780e9978d5005
IsmailM <ismail.moghul@gmail.com>
2014-09-25 10:47:39 +0100

    update

--- lib/GeneValidatorApp/PreRunValidation.rb
+++ lib/GeneValidatorApp/PreRunValidation.rb
@@ -1,6 +1,6 @@
 require 'logger'
 require 'fileutils'
-
+require 'yaml'
 
 LOG = Logger.new(STDOUT)
 LOG.formatter = proc do |severity, datetime, progname, msg|
@@ -11,41 +11,18 @@ LOG.level = Logger::INFO
 module GeneValidatorApp
   class Prerun
 
-    def self.prerun(db_root, tempdir)
+    def self.prerun(config_file, tempdir)
+      config                = YAML.load_file(config_file)
       dbs                   = {}
-      dbs[:dbs]             = scan_blast_database_directory(db_root)
+      dbs[:dbs]             = scan_blast_database_directory(config['database'])
+      dbs[:default_db]      = defaultdb(dbs[:dbs], config['default-database'])
       dbs[:non_default_dbs] = dbs[:dbs].clone
-      dbs[:default_db]      = choose_default(dbs[:dbs])
-      default_db_name       = dbs[:default_db].keys[0]
-      dbs[:non_default_dbs].delete(default_db_name)
-      puts # a blank line
-      puts 'Testing if Genevalidator (and all it\'s dependencies) are working.'
-      check_genevalidator_works(tempdir, default_db_name)
-      puts # a blank line
-      puts 'All pre-run tests have passed.'
-      puts # a blank line
+      dbs[:non_default_dbs].delete(config['default-database'])
+
+      assert_gv_installed_and_compatible
+      check_genevalidator_works(tempdir, config['default-database'])
+
       return dbs
-    end
-
-    ### Runs GeneValidator with a small test case... If GeneValidator exits with the
-    #   the right exit code, it is assumed that it works perfectly. This also tests
-    #   the Tempdir is writable....
-    def self.check_genevalidator_works(tempdir, default_db)
-      initial_tests = File.join(tempdir, 'initial_tests')
-      test_file     = File.join("#{File.dirname(__FILE__)}", '..', '..', 'public',
-                                'GeneValidator', 'initial_tests', 'initial_test.fa')
-
-      FileUtils.mkdir_p(initial_tests)
-      FileUtils.cp(test_file, initial_tests)
-
-      command    = 'Genevalidator -d ' + default_db + ' ' + File.join(initial_tests,
-                   'initial_test.fa')
-      %x(#{command})
-      unless $?.exitstatus == 0
-        raise IOError, "Genevalidator exited with the command code: #{exit}." \
-                       " It is possible that GeneValidator has not properly been "
-                       "installed."
-      end
     end
 
     ##### The below has been adapted from SequenceServer
@@ -87,7 +64,7 @@ module GeneValidatorApp
         next if line.empty?  # required for BLAST+ 2.2.22
         type, path, *title =  line.split(' ')
         type  = type.downcase
-        next unless type == 'protein'
+        next unless type == 'protein' # to ensure we only have protein dbs
         path  = path.freeze
         title = title.join(' ').freeze
         # # skip past all but alias file of a NCBI multi-part BLAST database
@@ -96,9 +73,8 @@ module GeneValidatorApp
           # logger.info(%|Found a multi-part database volume at #{path} - ignoring it.|)
           next
         end
-        db[title] = [path: path, type:type]
+        db[path] = [title: title, type:type]
         puts "Found #{type} database: #{title} at #{path}"
-        # logger.info("Found #{type} database: #{title} at #{path}")
       end
       db
     end
@@ -107,65 +83,66 @@ module GeneValidatorApp
       !(db_name.match(/.+\/\S+\d{2}$/).nil?)
     end
 
+    def self.defaultdb(databases, default_database)
+      default_db = {}
+      if databases.include?(default_database)
+        default_db[default_database] = databases[default_database]
+      end
+      # TODO: If not found, look for the db ensure that the db exists by checking the path...
+      return default_db
+    end
+
     ### Ensures that GV is installed and is of the correct version...
-    def self.assert_gv_installed_and_compatible()
-      unless command? 'genevalidator'
-        puts "*** Could not find GeneValidator. Please Confirm that you have "
+    def self.assert_gv_installed_and_compatible
+      puts # a blank line
+      puts "Testing whether GeneValidator is installed."
+      unless command?('genevalidator --version')
+        puts "*** Could not find the of GeneValidator. Please confirm that you have "
         puts "    GeneValidator installed and try again. "
         puts "    Please refer to ...Link... for more information."
         exit
       end
       ### TODO: Add the --version argument to GeneValidator
-      # version = %x|genevalidator -version|.split[1]
-      # unless version >= '1.0'
-      #   puts "*** Your GeneValidator version #{version} is outdated."
-      #   puts "    This App require GeneValidator version 1.0 or higher."
-      #   exit
-      # end
+      puts "Ensuring that the right version of GeneValidator is installed."
+      version = %x|genevalidator --version|
+      unless version.to_f >= 0.1
+        puts "*** Your GeneValidator (version #{version}) is outdated."
+        puts "    This App require GeneValidator version 0.1 or higher."
+        exit
+      end
     end
 
     # check if the given command exists and is executable
     # returns True if all is good.
-    def command?(command)
-      system("which #{command} > /dev/null 2>&1")
+    def self.command?(command)
+      %x|which #{command}|
+      return true if $?.exitstatus != 0
     end
 
-    def self.choose_default(databases)
-      default_db      = {}
-      db_titles = databases.keys
-      if db_titles.length == 1
-        default_db_name = db_titles[0]
-      else
-        puts # a blank line
-        puts "#{databases.length} databases found: "
-        db_titles.each_with_index { |db, idx| puts "*  #{idx + 1}: #{db}" }
-        puts # a blank line
-        puts "Please choose your default database. (Pick a number between 1 and #{db_titles.length}) "
-        print '> '
-        inp = $stdin.gets.chomp
-        input = inp.to_i
-        until (input <= db_titles.length && input >= 1)
-          puts # a blank line
-          puts "The input: '#{inp}' is not recognised - please try again."
-          puts # a blank line
-          db_titles.each_with_index { |db, idx| puts "#{idx + 1}: #{db}" }
-          puts # a blank line
-          puts "Please choose your default database. (Pick a number between 1 and #{db_titles.length}) "
-          print '> '
-          inp = $stdin.gets.chomp
-          input = inp.to_i
-        end
-        if input <= db_titles.length && input >= 1
-          db_number = input - 1
-          default_db_name = db_titles[db_number]
-        end
-        puts "You have chosen #{default_db_name} as your default database."
-        puts # a blank line
+    ### Runs GeneValidator with a small test case... If GeneValidator exits with the
+    #   the right exit code, it is assumed that it works perfectly. This also tests
+    #   the Tempdir is writable....
+    def self.check_genevalidator_works(tempdir, default_db)
+      puts 'Testing if Genevalidator (and all it\'s dependencies) are working.'
+
+      initial_tests = File.join(tempdir, 'initial_tests')
+      test_file     = File.join("#{File.dirname(__FILE__)}", '..', '..', 'public',
+                                'GeneValidator', 'initial_tests', 'initial_test.fa')
+
+      FileUtils.mkdir_p(initial_tests)
+      FileUtils.cp(test_file, initial_tests)
+
+      command    = 'Genevalidator -d ' + default_db + ' ' + File.join(initial_tests,
+                   'initial_test.fa')
+      %x(#{command})
+      unless $?.exitstatus == 0
+        raise IOError, "Genevalidator exited with the command code: #{exit}." \
+                       " It is possible that GeneValidator has not properly been "
+                       "installed."
       end
-      #Create a separate HASH for the default database
-      default_db[default_db_name] = databases[default_db_name]
-      # raise IOError,  if databases.include?(default_db_name)
-      return default_db
+      puts # a blank line
+      puts 'All pre-run tests have passed.'
+      puts # a blank line
     end
   end
 end
