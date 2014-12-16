1d6c0ed75d4229309f7934317db8fd12d3c70a25
IsmailM <ismail.moghul@gmail.com>
2014-12-16 09:36:09 +0000

    add tests

---
+++ spec/app_spec.rb
@@ -0,0 +1,84 @@
+require 'GeneValidatorApp'
+require 'rack/test'
+# require 'w3c_validators'
+
+module GeneValidatorApp
+  describe 'App' do
+    ENV['RACK_ENV'] = 'test'
+    include Rack::Test::Methods
+    # include W3CValidators
+
+    let 'root' do
+      GeneValidatorApp.root
+    end
+
+    let 'empty_config' do
+      File.join(root, 'spec', 'empty_config.yml')
+    end
+
+    let 'database_dir' do
+      File.join(root, 'spec', 'database')
+    end
+
+    let 'app' do
+      GeneValidatorApp.init(:config_file => empty_config,
+                            :database_dir => database_dir)
+    end
+
+    before :each do
+      app
+      @params = { 'seq' => 'AGCTAGCTAGCT',
+                  'validations' => '["lenc", "lenr", "dup", "merge", "align", "frame", "orf"]',
+                  'database' => Database.first.name}
+    end
+
+    it 'should start the app' do
+      get '/'
+      last_response.ok?.should == true 
+    end
+
+    it 'returns Bad Request (400) if no sequence is provided' do
+      @params.delete('seq')
+      post '/input', @params
+      last_response.status.should == 400
+    end
+
+    it 'returns Bad Request (400) if no validations is provided' do
+      @params.delete('validations')
+      post '/input', @params
+      last_response.status.should == 400
+    end
+
+    it 'returns Bad Request (400) if no database is provided' do
+      @params.delete('database')
+      post '/input', @params
+      last_response.status.should == 400
+    end
+
+    it 'returns OK (200) when sequence, validations and database name are provided' do
+      post '/input', @params
+      last_response.status.should == 200
+
+      @params[:result_link] = 'yes'
+      post '/input', @params
+      last_response.status.should == 200
+    end
+
+    it 'returns not found (404) when random url is posted against' do
+      post '/not_a_real_url', @params
+      last_response.status.should == 404
+    end
+
+    # it 'validate the html' do
+    #   get '/'
+    #   html = last_response.body
+    #   @html_validator = MarkupValidator.new
+    #   @html_validator.set_doctype!(:html5)
+
+    #   @html_validator.set_debug!(true)
+    #   results = @html_validator.validate_file(html)
+
+    #   puts results
+    # end    
+  end
+end
---
+++ spec/database_spec.rb
@@ -0,0 +1,40 @@
+require 'GeneValidatorApp'
+require 'GeneValidatorApp/database'
+require 'rspec'
+
+module GeneValidatorApp
+
+  describe 'Database' do
+
+    let 'root' do
+      GeneValidatorApp.root
+    end
+
+    let 'empty_config' do
+      File.join(root, 'spec', 'empty_config.yml')
+    end
+
+    let 'database_dir' do
+      File.join(root, 'spec', 'database')
+    end
+
+    let 'database_dir_no_db' do
+      File.join(root, 'spec', 'database', 'proteins', 'Cardiocondyla_obscurior')
+    end
+
+    let 'app' do
+      GeneValidatorApp.init(:config_file  => empty_config,
+                          :database_dir => database_dir)
+    end
+
+    it 'can tell BLAST+ databases in a directory' do
+
+    end
+
+    it 'can tell NCBI multipart database name' do
+      Database.multipart_database_name?('/home/ben/pd.ben/sequenceserver/db/nr.00').should be_true
+      Database.multipart_database_name?('/home/ben/pd.ben/sequenceserver/db/nr').should be_false
+      Database.multipart_database_name?('/home/ben/pd.ben/sequenceserver/db/img3.5.finished.faa.01').should be_true
+    end
+  end
+end
